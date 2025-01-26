e47f4a9313aad08dd1ebec912f8d92b0e091d870
nserekonajib <109471084+nserekonajib@users.noreply.github.com>
2025-01-26 19:31:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def send_email():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5555)
